04a2162b0886dfc3f3b26621e8833268b4cf07c5
ohmin <ohminato.oshima@gmail.com>
2022-05-07 22:04:55 +0900

    Implemented cmdlets

---
+++ CshPlgr.Cli/Collector/Cmdlet.cs
@@ -0,0 +1,40 @@
+﻿using static CshPlgr.Lib.API;
+using System.Management.Automation;
+
+namespace CshPlgr.Cli.Collector
+{
+    [Cmdlet("Collect", "PolytonicWords")]
+    public class Cmdlet : System.Management.Automation.Cmdlet
+    {
+        private SortedSet<string> allWords = new SortedSet<string>();
+
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string[]? Lines
+        {
+            get; set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (Lines != null && Lines.Length > 0)
+            {
+                foreach (var line in Lines)
+                {
+                    var words = CollectPolytonicWords(line);
+                    foreach (var word in words)
+                    {
+                        allWords.Add(word);
+                    }
+                }
+            }
+
+            foreach (var word in allWords)
+            {
+                WriteObject(word);
+            }
+        }
+    }
+}
---
+++ CshPlgr.Cli/Converter/Cmdlet.cs
@@ -0,0 +1,29 @@
+﻿using static CshPlgr.Lib.API;
+using System.Management.Automation;
+
+namespace CshPlgr.Cli.Converter
+{
+    [Cmdlet("ConvertTo", "Polytonic")]
+    public class Cmdlet : System.Management.Automation.Cmdlet
+    {
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string[]? Lines
+        {
+            get; set;
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (Lines != null && Lines.Length > 0)
+            {
+                foreach (var line in Lines)
+                {
+                    WriteObject(ConvertToPolytonic(line));
+                }
+            }
+        }
+    }
+}
